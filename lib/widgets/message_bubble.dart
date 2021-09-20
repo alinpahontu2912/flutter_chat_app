5fba5e1a22d7d9878c46c21ec5edbd5689b0b33b
stafy2912 <stafy2912@gmail.com>
2021-09-20 22:59:52 +0300

    added firebase notifications (no firebase functions :/ )

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -6,45 +8,61 @@ class MessageBubble extends StatelessWidget {
   final bool isMe;
   final Key key;
   final String username;
+  final String userImage;
 
-  MessageBubble(this.message, this.isMe, this.username, {this.key});
+  MessageBubble(this.message, this.isMe, this.username, this.userImage,
+      {this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
+      clipBehavior: Clip.none,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius:
-                isMe ? BorderRadius.circular(0) : BorderRadius.circular(10),
-          ),
-          width: 140,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                username,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                  color: isMe ? Colors.red : Colors.black,
-                ),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius:
+                    isMe ? BorderRadius.circular(0) : BorderRadius.circular(10),
               ),
-              Text(
-                message,
-                style: TextStyle(
-                  color: isMe ? Colors.red : Colors.black,
-                  fontWeight: FontWeight.w300,
-                ),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              width: 140,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 6),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    username,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      color: isMe ? Colors.red : Colors.black,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: isMe ? Colors.red : Colors.black,
+                      fontWeight: FontWeight.w300,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
               ),
-            ],
+            ),
+          ],
+        ),
+        Positioned(
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(userImage),
           ),
+          top: -10,
+          left: isMe ? null : 120,
+          right: isMe ? 120 : null,
         ),
       ],
     );
